717657eaf327b09309ef6c81314b3add5bfab934
poupouenpls <aurel.hasteur@gmail.com>
2025-02-17 20:42:17 +0100

    feat: ajout des dernieres fonctions

--- portfolio_terminal/src/App.jsx
+++ portfolio_terminal/src/App.jsx
@@ -12,29 +12,60 @@ const App = () => {
     switch(cmd.toLowerCase()) {
       case 'help':
         return [
-          'Commandes disponibles:',
-          'Whois - qui suis-je ?',
-          'Social - mes reseaux',
-          'Projets - mes projets sur github',
-          'Help - Vous savez deja ce que ca fait',
-          'Email - Evitez de me spammer please',
-          'Banniere - Afficher la banniere'
+          <table key="help">
+            <tbody>
+              <tr>
+                <td>Whois</td>
+                <td>Qui suis-je ?</td>
+              </tr>
+              <tr>
+                <td>Social</td>
+                <td>Mes reseaux</td>
+              </tr>
+              <tr>
+                <td>Projets</td>
+                <td>Mes projets sur github</td>
+              </tr>
+              <tr>
+                <td>Help</td>
+                <td>Vous savez deja ce que ca fait</td>
+              </tr>
+              <tr>
+                <td>Email</td>
+                <td>Evitez de me spammer please</td>
+              </tr>
+            </tbody>
+          </table>
         ]
       case 'whois':
-        return []
+        return [
+          <div key="phrase1">
+            <p>Salut, moi c&apos;est Aurélien! 👋</p>
+            <p>Je suis actuellement en formation de développeur web à la f@brique numérique 41.</p>
+            <p>Ma technologie préférée, c&apos;est ChatGPT.</p>
+            <p>Ahah je rigole.</p>
+            <p>Plus sérieusement, mes stacks sont : ReactJS, Node.js, MySQL, PHP.</p>
+            <p>À mes heures perdues, il m&apos;arrive de faire de la cybersécurité sur HackTheBox.</p>
+            <p>Vous voulez voir mon profil sur HackTheBox, vous pouvez taper social.</p>
+            <p>Pareil, si vous voulez voir mes projets, tapez social.</p>
+            <p>À bientôt 😎</p>
+          </div>
+        ]
       case 'social':
         return [
           <p key="github">Le lien de mon <a href="https://github.com/aurelienachard" target="_blank">github</a></p>,
-          <p key="linkedin">Le lien de mon <a href="https://www.linkedin.com/in/aurelien-achard-it/" target="_blank">Linkedin</a></p>
+          <p key="linkedin">Le lien de mon <a href="https://www.linkedin.com/in/aurelien-achard-it/" target="_blank">Linkedin</a></p>,
+          <p key="hackthebox">Le lien de mon <a href="https://www.linkedin.com/in/aurelien-achard-it/" target="_blank">Linkedin</a></p>
         ]
       case 'projets':
         return [
-
+          <p key="projet1">Projet<a href="https://github.com/aurelienachard/ecommerce_cafthe" target="_blank">cafthe</a></p>,
+          <p key="projet2">Projet<a href="https://github.com/aurelienachard/portfolio_terminal" target="_blank">portfolio</a></p>
         ]
       case 'email':
-        return []
-      case 'banniere':
-        return []
+        return [
+          <p key="mail">Mon email: <a href="mailto:contact@aurelienachard.com">contact@aurelienachard.com</a></p>
+        ]
       default:
         return [`Command not found: ${cmd}`]
     }
@@ -54,8 +85,8 @@ const App = () => {
 
   return (
     <div>
-      <h1>Bienvenue sur mon portfolio terminal.</h1>
-      <p>Pour la liste des commandes, type help.</p>
+      <h1>Bienvenue sur mon portfolio web interactif</h1>
+      <p>Pour obtenir une liste des commandes disponibles, tapez &quot;help&quot;.</p>
 
       <div>
         {command.map((cmd, index) => (
