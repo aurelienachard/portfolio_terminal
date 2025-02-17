a5684a54745d339fc9ad4f0451c16a192624a155
poupouenpls <aurel.hasteur@gmail.com>
2025-02-17 20:58:35 +0100

    feat: ajout du design

--- portfolio_terminal/src/App.jsx
+++ portfolio_terminal/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import './style.css'
 
 const App = () => {
   const [command, setCommand] = useState([])
@@ -12,34 +13,34 @@ const App = () => {
     switch(cmd.toLowerCase()) {
       case 'help':
         return [
-          <table key="help">
+          <table key="help" className="mt-4">
             <tbody>
               <tr>
-                <td>Whois</td>
-                <td>Qui suis-je ?</td>
+                <td className="border border-orange-500 p-2">Whois</td>
+                <td className="border border-orange-500 p-2">Qui suis-je ?</td>
               </tr>
               <tr>
-                <td>Social</td>
-                <td>Mes reseaux</td>
+                <td className="border border-orange-500 p-2">Social</td>
+                <td className="border border-orange-500 p-2">Mes reseaux</td>
               </tr>
               <tr>
-                <td>Projets</td>
-                <td>Mes projets sur github</td>
+                <td className="border border-orange-500 p-2">Projets</td>
+                <td className="border border-orange-500 p-2">Mes projets sur github</td>
               </tr>
               <tr>
-                <td>Help</td>
-                <td>Vous savez deja ce que ca fait</td>
+                <td className="border border-orange-500 p-2">Help</td>
+                <td className="border border-orange-500 p-2">Vous savez deja ce que ca fait</td>
               </tr>
               <tr>
-                <td>Email</td>
-                <td>Evitez de me spammer please</td>
+                <td className="border border-orange-500 p-2">Email</td>
+                <td className="border border-orange-500 p-2">Evitez de me spammer please</td>
               </tr>
             </tbody>
           </table>
         ]
       case 'whois':
         return [
-          <div key="phrase1">
+          <div key="phrase1" className="mt-4">
             <p>Salut, moi c&apos;est Aurélien! 👋</p>
             <p>Je suis actuellement en formation de développeur web à la f@brique numérique 41.</p>
             <p>Ma technologie préférée, c&apos;est ChatGPT.</p>
@@ -53,18 +54,18 @@ const App = () => {
         ]
       case 'social':
         return [
-          <p key="github">Le lien de mon <a href="https://github.com/aurelienachard" target="_blank">github</a></p>,
-          <p key="linkedin">Le lien de mon <a href="https://www.linkedin.com/in/aurelien-achard-it/" target="_blank">Linkedin</a></p>,
-          <p key="hackthebox">Le lien de mon <a href="https://www.linkedin.com/in/aurelien-achard-it/" target="_blank">Linkedin</a></p>
+          <p key="github" className="mt-4">Le lien de mon <a href="https://github.com/aurelienachard" target="_blank" className="underline">github</a></p>,
+          <p key="linkedin">Le lien de mon <a href="https://www.linkedin.com/in/aurelien-achard-it/" target="_blank" className="underline">Linkedin</a></p>,
+          <p key="hackthebox">Le lien de mon <a href="https://www.linkedin.com/in/aurelien-achard-it/" target="_blank" className="underline">Linkedin</a></p>
         ]
       case 'projets':
         return [
-          <p key="projet1">Projet<a href="https://github.com/aurelienachard/ecommerce_cafthe" target="_blank">cafthe</a></p>,
-          <p key="projet2">Projet<a href="https://github.com/aurelienachard/portfolio_terminal" target="_blank">portfolio</a></p>
+          <p key="projet1" className="mt-4">Projet <a href="https://github.com/aurelienachard/ecommerce_cafthe" target="_blank" className="underline">cafthe</a></p>,
+          <p key="projet2">Projet <a href="https://github.com/aurelienachard/portfolio_terminal" target="_blank" className="underline">portfolio</a></p>
         ]
       case 'email':
         return [
-          <p key="mail">Mon email: <a href="mailto:contact@aurelienachard.com">contact@aurelienachard.com</a></p>
+          <p key="mail" className="mt-4">Mon email: <a href="mailto:contact@aurelienachard.com">contact@aurelienachard.com</a></p>
         ]
       default:
         return [`Command not found: ${cmd}`]
@@ -84,13 +85,13 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>Bienvenue sur mon portfolio web interactif</h1>
+    <div className="bg-black text-orange-500 font-mono min-h-screen p-4">
+      <h1 className="text-xl">Bienvenue sur mon portfolio web interactif</h1>
       <p>Pour obtenir une liste des commandes disponibles, tapez &quot;help&quot;.</p>
 
       <div>
         {command.map((cmd, index) => (
-          <div key={index}>
+          <div key={index} className="mt-4">
             <p>{cmd}</p>
           </div>
         ))}
@@ -98,7 +99,7 @@ const App = () => {
 
       <form onSubmit={addNote}>
         <div>
-          user@OS: <input type="text" value={newCommande} onChange={handleNewCommand}/>
+          user@OS: <input type="text" value={newCommande} onChange={handleNewCommand} className="bg-black border border-orange-500 text-orange-500 p-2 mt-4"/>
         </div>
       </form>
     </div>
